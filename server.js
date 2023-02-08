3a5868aca21027532d714fcf94c9226622f7f855
Gold-Eman <goldeman500@gmail.com>
2023-02-08 09:16:22 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const PORT = process.env.PORT || 5000 
+ const express = require('express') 
+ const app = express() 
+  
+  
+ app.use(express.static('public')) 
+ app.set('view engine', 'ejs') 
+  
+  
+  
+  
+  
+ // ROUTERS 
+ app.get('/', (req,res)=>{ 
+    res.render('index') 
+  
+ }) 
+ app.get('/about',(req, res)=>{ 
+    res.render('about') 
+ }) 
+ app.get('/contact', (req, res)=> { 
+    res.render('contact') 
+ }) 
+ app.get('/services', (req, res)=> { 
+    res.render('services') 
+ }) 
+ app.get('/portfolio', (req,res)=> { 
+    res.render('portfolio') 
+ }) 
+ app.get('/blog', (req,res)=> { 
+    res.render('blog') 
+ }) 
+ app.get('/blog-details', (req,res)=> { 
+    res.render('blog-details') 
+ }) 
+ app.get('/pages', (req,res)=> { 
+    res.render('pages') 
+ }) 
+ app.listen(PORT, ()=>{ 
+    console.log('app running on local host 5000') 
+ })
